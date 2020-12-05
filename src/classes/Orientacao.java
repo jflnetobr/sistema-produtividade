f2de999bd229a20593ca76fa4355012382ece905
José Ferreira <jflneto.br@gmail.com>
2020-12-05 16:54:30 -0300

    Implementação da classe Orientacao

---
+++ src/classes/Orientacao.java
@@ -0,0 +1,9 @@
+package src.classes;
+
+import src.classes.enums.TipoProducao;
+
+public class Orientacao extends ProducaoAcademica {
+  public Orientacao(int id, String titulo, int anoPublicacao, TipoProducao tipo) {
+    super(id, titulo, anoPublicacao, tipo);
+  }
+}
